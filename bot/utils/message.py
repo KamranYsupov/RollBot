66d048400905ad5648806962468ad6ec192f5b11
KamranYsupov <kamrankek48@gmail.com>
2025-02-21 11:32:37 +0400

    Добавил отправку каритнки ролла из файлов

--- bot/utils/message.py
+++ bot/utils/message.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-
+from aiogram.types import FSInputFile
 from bot.keyboards.inline import get_inline_keyboard
 from bot.loader import bot
 from bot.models import Roll
@@ -9,11 +9,11 @@ async def send_roll_info(chat_id: int, roll: Roll):
         f'{roll.text}\n\n'
         f'<a href="{roll.link}">Ссылка</a>'
     )
+    photo = FSInputFile(f'web/media/{roll.image.name}')
 
-    photo_url = f'{settings.BASE_URL}{roll.image.url}'
     await bot.send_photo(
         chat_id=chat_id,
-        photo=photo_url,
+        photo=photo,
         caption=caption,
         reply_markup=get_inline_keyboard(
             buttons={'Попробовать ещё раз': 'random_roll'}
